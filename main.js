f4a12d7f670d47caf80edf077e4996bb9094ad74
RoulaHwaije <153372642+RoulaHwaije@users.noreply.github.com>
2024-02-15 21:40:58 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,93 @@
+// ********** fixed navbar ************
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  //  top link
+
+  if (scrollHeight > 150) {
+    topLink.classList.add("show-Top-link");
+  } else {
+    topLink.classList.remove("show-Top-link");
+  }
+});
+
+
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+   //linksContainer.classList.toggle("show-links");
+
+  const linksHeight = links.getBoundingClientRect().height + 75;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+  //console.log(links.getBoundingClientRect());
+});
+
+
+
+
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true; // Auto scroll
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector('.current');
+  current.classList.remove('current');
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add('current');
+  } else {
+    slides[0].classList.add('current');
+  }
+};
+
+
+const prevSlide = () => {
+  const current = document.querySelector('.current');
+  current.classList.remove('current');
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add('current');
+  } else {
+    slides[slides.length - 1].classList.add('current');
+  }
+};
+
+// Auto slide
+if (auto) {
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
+// Button events
+next.addEventListener('click', () => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+prev.addEventListener('click', () => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
